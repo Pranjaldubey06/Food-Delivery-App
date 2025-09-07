33a268f24c748af9d9593fdd6573055e54b7a08d
pranjal <pranjaldubey149@gmail.com>
2025-09-07 23:00:28 +0530

    add logo

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,30 +1,86 @@
-import React,{useEffect, useState} from 'react';
+// import React,{useEffect, useState} from 'react';
+// import Navbar from "../components/Navbar";
+// // import Home from "./components/Home";
+// import Information from "../components/Information";
+// import CategoryMenu from "../components/CategoryMenu";
+// import FoodItem from "../components/FoodItem";
+// import Cart from "../components/Cart";
+// import Banner from './Banner';
+
+
+// const Home = () => {
+  
+// useEffect(() => {
+//     document.title = "Food Ordering App";
+//   }, []);
+//   return (
+   
+//     <div>
+  
+//   <Navbar/>
+// <Banner/>
+// <Information/>
+// <CategoryMenu/>
+// <FoodItem/>
+// <Cart/>
+//     </div>
+//   )
+// }
+
+// export default Home
+import React, { useEffect, useState } from 'react';
 import Navbar from "../components/Navbar";
-// import Home from "./components/Home";
 import Information from "../components/Information";
 import CategoryMenu from "../components/CategoryMenu";
 import FoodItem from "../components/FoodItem";
 import Cart from "../components/Cart";
 import Banner from './Banner';
-
+import { Zoom } from 'react-awesome-reveal';
 
 const Home = () => {
-  
-useEffect(() => {
-    document.title = "Food Ordering App";
-  }, []);
-  return (
-   
-    <div>
-  
-  <Navbar/>
-<Banner/>
-<Information/>
-<CategoryMenu/>
-<FoodItem/>
-<Cart/>
-    </div>
-  )
-}
+  const [showSplash, setShowSplash] = useState(true);
 
-export default Home
+  useEffect(() => {
+    document.title = "Food Ordering App";
+
+    
+    const timer = setTimeout(() => {
+      setShowSplash(false);
+    },5000);
+
+    return () => clearTimeout(timer); 
+  }, []);
+
+  if (showSplash) {
+    return (
+      <div style={{ 
+        display: "flex", 
+        justifyContent: "center", 
+        alignItems: "center", 
+        height: "100vh", 
+        backgroundColor: "orange" 
+      }}>
+        <Zoom>      
+          <img 
+          src=" https://d3aa3s3yhl0emm.cloudfront.net/output/lx/design/180e98f4ead1425e9d75c1500d5b803e_1024x1024.jpg"
+          alt="Splash Screen" 
+          style={{ width: "500px", height: "auto" }} 
+        />
+        </Zoom>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <Navbar />
+      <Banner />
+      <Information />
+      <CategoryMenu />
+      <FoodItem />
+      <Cart />
+    </div>
+  );
+};
+
+export default Home;
--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -13,13 +13,13 @@ const Information = () => {
 
   return (
     <div className='p-4 py-4'>
-      <div className='bg-gradient-to-r from-orange-500 to-red-500 hover:from-orange-600 hover:to-red-600 '>
+      <div className='bg-gradient-to-r from-orange-500 to-red-500 hover:from-orange-600 hover:to-red-600  border rounded-lg'>
         <div className="text-center h-auto">
           <h3 className='font-semibold '>Hello..{userName}</h3>
           <h5 className='text-lg font-semibold'>{new Date().toUTCString().slice(0, 16)}</h5>
-          <h1 className='font-semibold text-white text-4xl underline'>
+          <h1 className='font-semibold text-white text-4xl '>
             RESTRO-<span className="font-bold text-white">FOOD</span> 
-            <span className='text-black '> WELCOMES</span> YOU:
+            <span className='text-black '> WELCOMES</span> YOU
           </h1>
           <div className="flex justify-center">
           <IoFastFoodOutline size={52} className='text-black' />
